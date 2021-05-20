38c6efd4ade9ba6203ba5944e6817e8fa20224e4
kamil <kamilski81@gmail.com>
2021-05-20 08:24:58 -0500

    Bold nav links.

--- src/components/SectionNavbar.tsx
+++ src/components/SectionNavbar.tsx
@@ -4,9 +4,9 @@ import {NavLink} from "react-router-dom";
 export default function SectionNavbar(props: any) {
     return <React.Fragment>
         <div>
-            <NavLink to="/">Home</NavLink>
+            <NavLink exact to="/">Home</NavLink>
             &nbsp; | &nbsp;
-            <NavLink to="/exercises">Exercises</NavLink>
+            <NavLink exact to="/exercises">Exercises</NavLink>
         </div>
     </React.Fragment>
 }
