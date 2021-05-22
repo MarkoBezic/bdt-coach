1df6e3d1465a1a798d5128a6af9c14a79d5fc629
kamil <kamilski81@gmail.com>
2021-05-22 17:26:30 -0500

    CRUD for exercises.

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,117 +1,119 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useEffect} from 'react';
 import CRUDTable,
-{
-  Fields,
-  Field,
-  CreateForm,
-  UpdateForm,
-  DeleteForm,
+{Fields, Field, CreateForm, UpdateForm, DeleteForm,
 } from 'react-crud-table';
 
 // Component's Base CSS
 import '../index.css';
 import './AdminPage.css';
 import SectionNavbar from "../components/SectionNavbar";
-
-
-const DescriptionRenderer = ({ field }) => <textarea {...field} />;
-
-let tasks = [
-  {
-    id: 1,
-    title: 'Create an example',
-    description: 'Create an example of how to use the component',
-  },
-  {
-    id: 2,
-    title: 'Improve',
-    description: 'Improve the component!',
-  },
-];
-
-const SORTERS = {
-  NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
-  NUMBER_DESCENDING: mapper => (a, b) => mapper(b) - mapper(a),
-  STRING_ASCENDING: mapper => (a, b) => mapper(a).localeCompare(mapper(b)),
-  STRING_DESCENDING: mapper => (a, b) => mapper(b).localeCompare(mapper(a)),
-};
-
-const getSorter = (data) => {
-  const mapper = x => x[data.field];
-  let sorter = SORTERS.STRING_ASCENDING(mapper);
-
-  if (data.field === 'id') {
-    sorter = data.direction === 'ascending' ?
-      SORTERS.NUMBER_ASCENDING(mapper) : SORTERS.NUMBER_DESCENDING(mapper);
-  } else {
-    sorter = data.direction === 'ascending' ?
-      SORTERS.STRING_ASCENDING(mapper) : SORTERS.STRING_DESCENDING(mapper);
-  }
-
-  return sorter;
-};
-
-let count = tasks.length;
-const service = {
-  fetchItems: (payload) => {
-    let result = Array.from(tasks);
-    result = result.sort(getSorter(payload.sort));
-    return Promise.resolve(result);
-  },
-  create: (task) => {
-    count += 1;
-    tasks.push({
-      ...task,
-      id: count,
-    });
-    return Promise.resolve(task);
-  },
-  update: (data) => {
-    const task = tasks.find(t => t.id === data.id);
-    task.title = data.title;
-    task.description = data.description;
-    return Promise.resolve(task);
-  },
-  delete: (data) => {
-    const task = tasks.find(t => t.id === data.id);
-    tasks = tasks.filter(t => t.id !== task.id);
-    return Promise.resolve(task);
-  },
-};
-
-const styles = {
-  container: { margin: 'auto', width: 'fit-content' },
-};
+import {LOCAL_STORAGE_KEY_EXERCISES} from "../hooks/useLocalStorage";
 
 export default function AdminPage() {
+
+  let storageEx = localStorage.getItem(LOCAL_STORAGE_KEY_EXERCISES)
+  let exercises = JSON.parse(storageEx) || []
+
+  const saveExercises = (exercises) => {
+    const exercisesAsString = JSON.stringify(exercises)
+    localStorage.setItem(LOCAL_STORAGE_KEY_EXERCISES, exercisesAsString);
+  }
+
+  const SORTERS = {
+    NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
+    NUMBER_DESCENDING: mapper => (a, b) => mapper(b) - mapper(a),
+    STRING_ASCENDING: mapper => (a, b) => mapper(a).localeCompare(mapper(b)),
+    STRING_DESCENDING: mapper => (a, b) => mapper(b).localeCompare(mapper(a)),
+  };
+
+  const getSorter = (data) => {
+    const mapper = x => x[data.field];
+    let sorter = SORTERS.STRING_ASCENDING(mapper);
+
+    if (data.field === 'id') {
+      sorter = data.direction === 'ascending' ?
+        SORTERS.NUMBER_ASCENDING(mapper) : SORTERS.NUMBER_DESCENDING(mapper);
+    } else {
+      sorter = data.direction === 'ascending' ?
+        SORTERS.STRING_ASCENDING(mapper) : SORTERS.STRING_DESCENDING(mapper);
+    }
+
+    return sorter;
+  };
+
+  let count = exercises.length;
+  const service = {
+    fetchItems: (payload) => {
+      storageEx = localStorage.getItem(LOCAL_STORAGE_KEY_EXERCISES)
+      exercises = JSON.parse(storageEx) || []
+
+      let result = Array.from(exercises);
+      result = result.sort(getSorter(payload.sort));
+      return Promise.resolve(result);
+    },
+    create: (exercise) => {
+      count += 1;
+      exercises.push({
+        ...exercise,
+        id: count,
+      });
+
+      saveExercises(exercises)
+
+      return Promise.resolve(exercise)
+    },
+    update: (data) => {
+      const exercise = exercises.find(t => t.id === data.id);
+      exercise.name = data.name;
+      exercise.rep_duration = data.rep_duration;
+      exercise.color = data.color;
+
+      saveExercises(exercises)
+
+      return Promise.resolve(exercise)
+    },
+    delete: (data) => {
+      const exercise = exercises.find(t => t.id === data.id);
+      const exercisesPostProcess = exercises.filter(t => t.id !== exercise.id);
+
+      saveExercises(exercisesPostProcess)
+
+      return Promise.resolve(exercise)
+    },
+  };
+
+  const styles = {
+    container: { margin: 'auto', width: 'fit-content' },
+  };
 
   return <div style={styles.container}>
     <SectionNavbar />
     <CRUDTable caption="Exercises" fetchItems={payload => service.fetchItems(payload)}>
       <Fields>
-        <Field name="id" label="Id" hideInCreateForm readOnly/>
-        <Field name="title" label="Title" placeholder="Title"/>
-        <Field name="description" label="Description" render={DescriptionRenderer}/>
+        <Field name="id" label="Id" hideInCreateForm hideInUpdateForm readOnly/>
+        <Field name="name" label="Name" placeholder="Name"/>
+        <Field name="rep_duration" label="Rep Duration (sec.)" placeholder="How long does this rep last?"/>
+        <Field name="color" label="Color" placeholder="#F00"/>
       </Fields>
-      <CreateForm title="Task Creation" message="Create a new task!" trigger="Create Task"
-        onSubmit={task => service.create(task)} submitText="Create"
+
+      <CreateForm title="Exercise Creation" message="Create a new exercise!" trigger="Create Exercise"
+        onSubmit={exercise => service.create(exercise)} submitText="Create"
         validate={(values) => {
           const errors = {};
-          if (!values.title) {
-            errors.title = 'Please, provide task\'s title';
+          if (!values.name) {
+            errors.name = 'Please, provide exercise\'s name';
           }
 
-          if (!values.description) {
-            errors.description = 'Please, provide task\'s description';
+          if (!values.rep_duration) {
+            errors.rep_duration = 'Please, provide the rep duration';
           }
 
           return errors;
         }}
       />
 
-      <UpdateForm title="Task Update Process" message="Update task" trigger="Update"
-        onSubmit={task => service.update(task)} submitText="Update"
+      <UpdateForm title="Exercise Update Process" message="Update exercise" trigger="Update"
+        onSubmit={exercise => service.update(exercise)} submitText="Update"
         validate={(values) => {
           const errors = {};
 
@@ -119,20 +121,20 @@ export default function AdminPage() {
             errors.id = 'Please, provide id';
           }
 
-          if (!values.title) {
-            errors.title = 'Please, provide task\'s title';
+          if (!values.name) {
+            errors.name = 'Please, provide exercise\'s name';
           }
 
-          if (!values.description) {
-            errors.description = 'Please, provide task\'s description';
+          if (!values.rep_duration) {
+            errors.rep_duration = 'Please, provide exercise\'s rep duration';
           }
 
           return errors;
         }}
       />
 
-      <DeleteForm title="Task Delete Process" message="Are you sure you want to delete the task?" trigger="Delete"
-        onSubmit={task => service.delete(task)} submitText="Delete"
+      <DeleteForm title="Exercise Delete Process" message="Are you sure you want to delete the exercise?" trigger="Delete"
+        onSubmit={exercise => service.delete(exercise)} submitText="Delete"
         validate={(values) => {
           const errors = {};
           if (!values.id) {
